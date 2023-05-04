e9813a3c64acee950c8155f9d88c5c6cfc6d2139
userAugustos <felipe_augustos84@outlook.com.br>
2023-05-03 22:28:26 -0300

    feat: adding the js to the typewriter, now is much more natural

---
+++ js/typewrite.js
@@ -0,0 +1,44 @@
+
+const messages = ["Este é um texto feito usando js, para dar mais realismo ao efeito", "e contento vários textos"]
+const speed = 90
+let textPosition = 0
+const button = document.querySelector(".init")
+const element = document.querySelector("#message");
+
+
+function typewriter(message, shouldErase) {
+    element.innerHTML = message.substring(0, textPosition)
+    if(textPosition++ !== message.length) {
+        setTimeout(() => typewriter(message), speed)
+    }
+
+//     erase
+//     ok, there is a error here, the shouldErase is not been passed correctly.
+    if(shouldErase && textPosition >= message.length){
+        setTimeout(() => erase(message, shouldErase), 2000)
+    }
+}
+
+function erase(message) {
+    setInterval(() => {
+        if(textPosition === 0) {
+            return null
+        }
+        console.debug('it happened')
+        element.textContent = message.substring(0, textPosition - 1)
+        textPosition -= 1;
+    }, speed)
+}
+
+function manyWords() {
+    if(messages.length++ === -1) {
+        return null
+    }
+
+    typewriter(messages[0], true)
+    // typewriter(messages[1], false)
+}
+button.addEventListener('click', erase)
+
+window.addEventListener("load", manyWords)
+
